a444aeb4635e5797aa19a1fa2e113f95369b910f
blaircalderwood@live.co.uk <blaircalderwood@live.co.uk>
2016-10-27 13:36:42 +0100

    Switched ChartsJS to Angular-ChartsJS and placed charts inside an ng-repeat for brevity

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ var portfolioApp = angular.module('myApp', [
     'ngRoute',
     'angular-inview',
     'ui.bootstrap',
+    'chart.js',
     'duScroll',
     'projects',
     'contactme',
@@ -20,6 +21,15 @@ var portfolioApp = angular.module('myApp', [
 portfolioApp.controller('MainController', function ProjectsController($scope) {
 
     var mainNav = angular.element('#myNav');
+    var items;
+
+    // Requires document ready to ensure all elements are loaded into DOM before searching by class
+    angular.element(document).ready(function () {
+        items = angular.element(document.getElementsByClassName("main-section"));
+    });
+
+    if (window.innerWidth < 480)$scope.chartsHeight = "10vh";
+    else $scope.chartsHeight = "40vh";
 
     $scope.sectionClass = [];
     $scope.sectionClass.push('isvisible');
@@ -31,13 +41,17 @@ portfolioApp.controller('MainController', function ProjectsController($scope) {
         }
     });
 
+    function changeChartHeight() {
+        if (window.innerWidth < 480)$scope.chartsHeight = "80vh";
+        else $scope.chartsHeight = "40vh";
+    }
+
     $scope.sectionName = 'Web Developer';
     $scope.nextSection = '#timelineSection';
 
-    var items = angular.element(".main-section");
-
     window.addEventListener("scroll", changeSection);
     window.addEventListener("load", changeSection);
+    //window.addEventListener("resize", changeChartHeight);
 
     function changeSection() {
 
@@ -73,111 +87,83 @@ portfolioApp.controller('MainController', function ProjectsController($scope) {
 });
 
 // From http://stackoverflow.com/questions/37674113/how-to-track-if-element-is-visible-on-screen-with-same-class-when-scrolling-with
-    $.fn.isOnScreen = function () {
-        var win = $(window);
-        var viewport = {
-            top: win.scrollTop(),
-            left: win.scrollLeft()
-        };
-        // These lines have been adapted to only look at top 50% of screen
-        viewport.right = viewport.left + (win.width() * 0.5);
-        viewport.bottom = viewport.top + (win.height() * 0.5);
-
-        var bounds = this.offset();
-        bounds.right = bounds.left + this.outerWidth();
-        bounds.bottom = bounds.top + this.outerHeight();
-        return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+$.fn.isOnScreen = function () {
+    var win = $(window);
+    var viewport = {
+        top: win.scrollTop(),
+        left: win.scrollLeft()
     };
+    // These lines have been adapted to only look at top 50% of screen
+    viewport.right = viewport.left + (win.width() * 0.5);
+    viewport.bottom = viewport.top + (win.height() * 0.5);
 
-function createCharts() {
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+};
 
-    var chartsData = [
-        {
-            chart: '',
-            section: 'Languages',
-            ctx: angular.element("#languagesChart")[0].getContext("2d"),
-            labelNames: ["        JavaScript", "HTML", "CSS", "Python", "Java"],
-            data: [10, 8, 8, 10, 5],
-            colours: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(0, 191, 255, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(255, 127 ,80, 1)'
-            ]
-        },
-
-        {
-            chart: '',
-            section: 'Technologies',
-            ctx: document.getElementById("technologiesChart").getContext("2d"),
-            labelNames: ["            Django", "Angular", "jQuery", "LESS", "Phaser", "Bootstrap"],
-            data: [10, 5, 8, 7, 6, 9],
-            colours: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(0, 191, 255, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(255, 127, 80, 1)',
-                'rgba(186, 85, 211, 1)'
-            ]
-        }
-    ];
+portfolioApp.controller('ChartsController', function ChartsController($scope) {
 
     var labels = [];
-    labels[0] = 'Least Experience';
+    labels[0] = 'Inexperienced';
     for (var i = 1; i < 10; i++) {
         labels[i] = '';
     }
-    labels[10] = 'Most Experience     ';
+    labels[10] = 'Experienced';
 
-    for (var index = 0; index < chartsData.length; index++) {
+    $scope.charts = [
+        {
+            title: "Languages",
+            id: "languagesSection",
+            labels: ["JavaScript", "HTML", "CSS", "Python", "Java"],
+            data: [10, 8, 8, 10, 5]
+        },
+        {
+            title: "Technologies",
+            id: "technologiesSection",
+            labels: ["Django", "Angular", "jQuery", "LESS", "Phaser", "Bootstrap"],
+            data: [10, 5, 8, 7, 6, 9]
+        }
+    ];
 
-        var chrt = chartsData[index];
+    $scope.chartOptions = {
 
-        chrt.chart = new Chart(chrt.ctx, {
-            type: 'horizontalBar',
-            data: {
-                labels: chrt.labelNames,
-                datasets: [{
-                    data: chrt.data,
-                    backgroundColor: chrt.colours
-                }]
-            },
-            options: {
+        tooltips: {
+            enabled: false
+        },
 
-                tooltips: {
-                    enabled: false
+        legend: {display: false},
+        scales: {
+            xAxes: [{
+                ticks: {
+                    maxRotation: 0,
+                    minRotation: 0,
+                    fontSize: 14,
+                    fontColor: '#fff',
+                    beginAtZero: true,
+                    callback: function (value, index, values) {
+                        return labels[value];
+                    }
                 },
+                gridLines: {display: false}
+            }],
+            yAxes: [{
+                ticks: {
+                    fontSize: 18,
+                    fontColor: '#fff'
+                },
+                gridLines: {display: false}
+            }]
+        }
+    };
 
-                legend: {display: false},
-                scales: {
-                    xAxes: [{
-                        ticks: {
-                            maxRotation: 0,
-                            minRotation: 0,
-                            fontSize: 18,
-                            fontColor: '#fff',
-                            beginAtZero: true,
-                            callback: function (value, index, values) {
-                                return labels[value];
-                            }
-                        },
-                        gridLines: {display: false}
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            fontSize: 18,
-                            fontColor: '#fff'
-                        },
-                        gridLines: {display: false}
-                    }]
-                }
-            }
-        });
-
-    }
-
-}
-
-createCharts();
+    Chart.defaults.global.colors = [
+                '#FF6384',
+                '#00BFFF',
+                '#FFCE56',
+                '#4BC0C0',
+                '#FF7F50',
+                '#BA55D3'
+            ]
+});
